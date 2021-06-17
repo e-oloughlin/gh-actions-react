3fcb4e41a5ac24d8fded53073a55466290a1dc69
Eoghan O'Loughlin <eoghanoloughlin@gmail.com>
2021-06-17 21:36:31 +0100

    Change text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Waaaaaaa</p>
+        <p>What the fuck</p>
         <a
           className="App-link"
           href="https://reactjs.org"
