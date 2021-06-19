86556e105f035f2f60cb710e84309d24f6a78a30
Eoghan O'Loughlin <eoghanoloughlin@gmail.com>
2021-06-19 18:04:40 +0100

    chore: update semantic release config

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  branches: 'main',
-  repositoryUrl: 'https://github.com/e-oloughlin/gh-actions-react.git',
+  branches: ['main'],
+  dryRun: true,
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
