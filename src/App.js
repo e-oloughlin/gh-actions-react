836fc3cbfaa9661b0cef7c245d2372d5536cdbcf
Eoghan O'Loughlin <eoghanoloughlin@gmail.com>
2021-06-19 19:05:18 +0100

    fix: change text again

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Why is it not working!
+          Use react
         </a>
       </header>
     </div>
